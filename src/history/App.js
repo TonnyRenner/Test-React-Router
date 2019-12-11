9417a6c5b38d24814016cf5fbfbe887961b01fda
JingPing.Liu <tonny_renner@163.com>
2019-12-11 17:53:03 +0800

    测试

--- src/history/App.js
+++ src/history/App.js
@@ -2,14 +2,37 @@ import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 
 class APP extends PureComponent {
+  onPushState = () => {
+    this.props.history.push({
+      pathname: '/repos',
+      state: {
+        id: 3
+      }
+    });
+  }
+
+  onReplaceState = () => {
+    this.props.history.replace({
+      pathname: '/repos',
+      state: {
+        id: 2
+      }
+    });
+  }
+
   render() {
     console.log('APP, this.props.history', this.props.history);
     return (
       <div>
         <h2>APP</h2>
+        <button onClick={this.onPushState}>push to repos.</button>
+        <button onClick={this.onReplaceState}>replace to repos.</button>
         <p>
           <Link to="/about/12?pageNo=1">跳转至About</Link>
         </p>
+        {/* <p>
+          <Link to="/repos/12?pageNo=1">跳转至Repos</Link>
+        </p> */}
       </div>
     );
   }
--- src/history/index.js
+++ src/history/index.js
@@ -1,38 +1,10 @@
-// import React, { Component } from 'react';
-// import { render } from 'react-dom';
-// import {
-//   // HashRouter as Router,
-//   // MemoryRouter as Router,
-//   BrowserRouter as Router,
-//   Route
-// } from 'react-router-dom';
-// import {
-//   createBrowserHistory, createMemoryHistory, createHashHistory
-// } from 'history';
-
-// import App from './App';
-// import Repos from './Repos';
-// import About from './About';
-
-// render((
-//   <Router>
-//     <Route path="/" component={App} />
-//     <Route path="/repos/:id" component={Repos} />
-//     <Route path="/about/:id" component={About} />
-//   </Router>
-// ), document.getElementById('application'));
-
 import React, { PureComponent } from 'react';
-// import { render } from 'react-dom';
 import {
-  HashRouter as Router,
+  // HashRouter as Router,
   // MemoryRouter as Router,
-  // BrowserRouter as Router,
+  BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-// import {
-//   createBrowserHistory, createMemoryHistory, createHashHistory
-// } from 'history';
 
 import App from './App';
 import Repos from './Repos';
@@ -44,8 +16,8 @@ class History extends PureComponent {
     return (
       <Router>
         <Route path="/" component={App} />
-        <Route path="/repos/:id" component={Repos} />
         <Route path="/about/:id" component={About} />
+        <Route path="/repos" component={Repos} />
       </Router>
     );
   }
